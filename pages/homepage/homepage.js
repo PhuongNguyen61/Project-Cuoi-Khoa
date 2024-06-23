ef0a3224ddc2fcad9d99cf0ebe7181228ab13269
PhuongNguyen61 <phuongnguyen.612003@gmail.com>
2024-06-23 17:25:23 +0700

    change repo

--- pages/homepage/homepage.js
+++ pages/homepage/homepage.js
@@ -1,24 +1,27 @@
 const productInforFrame1 = [
   {
     id: 1,
-    src: "../img/rangsayi.png",
+    srcProduct: "../img-pages/rangsayi.png",
     frame1CateItemTitle: "Cà Phê I - Khát Vọng Khởi Nghiệp",
     frame1ProductCardPrice: "80.000đ",
     frame1ProductCardScore: "4.8",
+    srcStar: "../icon-pages/star-svgrepo-com.svg",
   },
   {
     id: 2,
-    src: "../img/rangsaynau.png",
+    srcProduct: "../img-pages/rangsaynau.png",
     frame1CateItemTitle: "Cà Phê S - Chinh Phục Thành Công",
     frame1ProductCardPrice: "90.000đ",
     frame1ProductCardScore: "4.9",
+    srcStar: "../icon-pages/star-svgrepo-com.svg",
   },
   {
     id: 3,
-    src: "../img/rangsays.png",
+    srcProduct: "../img-pages/rangsays.png",
     frame1CateItemTitle: "Cà Phê Nâu - Sức Sống Đại Ngàn",
     frame1ProductCardPrice: "100.000đ",
     frame1ProductCardScore: "5.0",
+    srcStar: "../icon-pages/star-svgrepo-com.svg",
   },
 ];
 const frame1Item = document.getElementById("frame1-item");
@@ -29,7 +32,7 @@ for (let i = 0; i < productInforFrame1.length; i++) {
                         <a href="">
                             <div class="frame1-cate-item">
                                 <div class="frame1-cate-img">
-                                    <img src="${productInforFrame1[i].src}" alt="" class="frame1-img">
+                                    <img src="${productInforFrame1[i].srcProduct}" alt="" class="frame1-img">
                                 </div>
 
                                 <div class="frame1-cate-item-info">
@@ -37,7 +40,7 @@ for (let i = 0; i < productInforFrame1.length; i++) {
                                     <div class="frame1-product-card-row">
                                         <p class="frame1-product-card-price">${productInforFrame1[i].frame1ProductCardPrice}</p>
                                         <p class="frame1-product-card-score">${productInforFrame1[i].frame1ProductCardScore}</p>
-                                        <img src="../icon/star-svgrepo-com.svg" alt="" class="frame1-product-card-star">
+                                        <img src="${productInforFrame1[i].srcStar}" alt="" class="frame1-product-card-star">
                                     </div>
                                 </div>
                             </div>
@@ -50,53 +53,56 @@ for (let i = 0; i < productInforFrame1.length; i++) {
 const productInforFrame2 = [
   {
     id: 4,
-    src: "../img/sangtao1.png",
+    srcProduct: "../img-pages/sangtao1.png",
     frame2CateItemTitle: "Sáng Tạo 1",
     frame2CateItemContent: "Thơm dịu nhẹ, vị đậm đà",
     frame2ProductCardPrice: "85.000 VNĐ",
     frame2ProductCardScore: "4.7",
+    srcStar: "../icon-pages/star-svgrepo-com.svg",
   },
   {
     id: 5,
-    src: "../img/sangtao2.png",
+    srcProduct: "../img-pages/sangtao2.png",
     frame2CateItemTitle: "Sáng Tạo 2",
     frame2CateItemContent: "Thơm dịu nhẹ, vị hài hòa",
     frame2ProductCardPrice: "90.000 VNĐ",
     frame2ProductCardScore: "4.9",
+    srcStar: "../icon-pages/star-svgrepo-com.svg",
   },
   {
     id: 6,
-    src: "../img/sangtao3.png",
+    srcProduct: "../img-pages/sangtao3.png",
     frame2CateItemTitle: "Sáng Tạo 3",
     frame2CateItemContent: "Rất thơm, vị đắng nhẹ, chua thanh",
     frame2ProductCardPrice: "95.000 VNĐ",
     frame2ProductCardScore: "4.8",
+    srcStar: "../icon-pages/star-svgrepo-com.svg",
   },
   {
     id: 7,
-    src: "../img/sangtao4.png",
+    srcProduct: "../img-pages/sangtao4.png",
     frame2CateItemTitle: "Sáng Tạo 4",
     frame2CateItemContent: "Thơm lâu, vị cân bằng, thế chất đậm",
     frame2ProductCardPrice: "100.000 VNĐ",
     frame2ProductCardScore: "5.0",
+    srcStar: "../icon-pages/star-svgrepo-com.svg",
   },
 ];
-
 const frame2Item = document.getElementById("frame2-item");
 
 for (let i = 0; i < productInforFrame2.length; i++) {
   frame2Item.innerHTML += `
-    <div class="frame2-col">
-                        <a href="../product/product.html">
+  <div class="frame2-col">
+                        <a href="">
                             <div class="frame2-cate-item">
-                                <img src="${productInforFrame2[i].src}" alt="" class="frame2-img">
+                                <img src="${productInforFrame2[i].srcProduct}" alt="" class="frame2-img">
                                 <div class="frame2-cate-item-info">
-                                    <h3 class="frame2-cate-item-title">${productInforFrame2[i].frame2CateItemTitle}</h3>
+                                    <h2 class="frame2-cate-item-title">${productInforFrame2[i].frame2CateItemTitle}</h2>
                                     <p class="frame2-cate-item-content">${productInforFrame2[i].frame2CateItemContent}</p>
                                     <div class="frame2-product-card-row">
                                         <p class="frame2-product-card-price">${productInforFrame2[i].frame2ProductCardPrice}</p>
                                         <p class="frame2-product-card-score">${productInforFrame2[i].frame2ProductCardScore}</p>
-                                        <img src="../icon/star-svgrepo-com.svg" alt="" class="frame2-product-card-star">
+                                        <img src="${productInforFrame2[i].srcStar}" alt="" class="frame2-product-card-star">
                                     </div>
                                 </div>
                             </div>
@@ -109,35 +115,39 @@ for (let i = 0; i < productInforFrame2.length; i++) {
 const productInforFrame3 = [
   {
     id: 8,
-    src: "../img/chephin1.png",
+    srcProduct: "../img-pages/chephin1.png",
     frame3CateItemTitle: "Chế Phin 1",
     frame3CateItemContent: "Thơm dịu nhẹ, vị đậm đà",
     frame3ProductCardPrice: "85.000 VNĐ",
     frame3ProductCardScore: "4.7",
+    srcStar: "../icon-pages/star-svgrepo-com.svg",
   },
   {
     id: 9,
-    src: "../img/chephin2.png",
+    srcProduct: "../img-pages/chephin2.png",
     frame3CateItemTitle: "Chế Phin 2",
     frame3CateItemContent: "Thơm dịu nhẹ, vị hài hòa",
     frame3ProductCardPrice: "90.000 VNĐ",
     frame3ProductCardScore: "4.9",
+    srcStar: "../icon-pages/star-svgrepo-com.svg",
   },
   {
     id: 10,
-    src: "../img/chephin3.png",
+    srcProduct: "../img-pages/chephin3.png",
     frame3CateItemTitle: "Chế Phin 3",
     frame3CateItemContent: "Rất thơm, vị đắng nhẹ, chua thanh",
     frame3ProductCardPrice: "95.000 VNĐ",
     frame3ProductCardScore: "4.8",
+    srcStar: "../icon-pages/star-svgrepo-com.svg",
   },
   {
     id: 11,
-    src: "../img/chephin4.png",
+    srcProduct: "../img-pages/chephin4.png",
     frame3CateItemTitle: "Chế phin 4",
     frame3CateItemContent: "Thơm lâu, vị cân bằng, thế chất đậm",
     frame3ProductCardPrice: "100.000 VNĐ",
     frame3ProductCardScore: "5.0",
+    srcStar: "../icon-pages/star-svgrepo-com.svg",
   },
 ];
 const frame3Item = document.getElementById("frame3-item");
@@ -147,14 +157,14 @@ for (let i = 0; i < productInforFrame3.length; i++) {
   <div class="frame3-col">
                         <a href="">
                             <div class="frame3-cate-item">
-                                <img src="${productInforFrame3[i].src}" alt="" class="frame3-img">
+                                <img src="${productInforFrame3[i].srcProduct}" alt="" class="frame3-img">
                                 <div class="frame3-cate-item-info">
                                     <h3 class="frame3-cate-item-title">${productInforFrame3[i].frame3CateItemTitle}</h3>
                                     <p class="frame3-cate-item-content">${productInforFrame3[i].frame3CateItemContent}</p>
                                     <div class="frame3-product-card-row">
                                         <p class="frame3-product-card-price">${productInforFrame3[i].frame3ProductCardPrice}</p>
                                         <p class="frame3-product-card-score">${productInforFrame3[i].frame3ProductCardScore}</p>
-                                        <img src="../icon/star-svgrepo-com.svg" alt="" class="frame3-product-card-star">
+                                        <img src="${productInforFrame3[i].srcStar}" alt="" class="frame3-product-card-star">
                                     </div>
                                 </div>
                             </div>
